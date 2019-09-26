e68ffc12a6d78b9f3ccfc1a98d7510762676e15c
Kdaw <s6089158@tees.ac.uk>
2019-09-26 16:51:19 +0100

    Added in drilldown for products

---
+++ Producks.Web/Controllers/StoreController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Producks.Data;
+
+namespace Producks.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly StoreDb _context;
+
+        public StoreController(StoreDb context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .Where(p => p.CategoryId == id)
+                .ToListAsync();
+            if(products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+    }
+}
