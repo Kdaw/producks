de3416ab1be1ac77d7437f62c856fab0f65410e9
Kdaw <s6089158@tees.ac.uk>
2019-10-03 15:34:55 +0100

    Added UnderCutters DTOs

--- Producks.Web/Models/UnderCutterProductDto.cs
+++ Producks.Web/Models/UnderCutterProductDto.cs
@@ -9,5 +9,14 @@ namespace Producks.Web.Models
     {
         public int Id { get; set; }
         public string Ean { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool InStock { get; set; }
+        public DateTime ExpectedRestock { get; set; }
     }
 }
